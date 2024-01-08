9e98cb164061fb973fbbb53eb09dfbf17149a60d
james.hsueh77 <james.hsueh@titansoft.com.sg>
2024-01-09 00:59:41 +0800

    feat: add calculation for 1 hour and 1 day

--- tdd-dojo-parking-service/ParkingServiceTests.cs
+++ tdd-dojo-parking-service/ParkingServiceTests.cs
@@ -30,17 +30,35 @@ public class ParkingServiceTests
         var fee = _parkingService.CalculateFee(arriveTime, leaveTime);
         Assert.AreEqual(30, fee);
     }
+    
+    [Test]
+    public void should_be_60()
+    {
+        var arriveTime = new DateTime(2024, 1, 9, 0, 0, 0, 0, DateTimeKind.Utc);
+        var leaveTime = new DateTime(2024, 1, 9, 1, 0, 0, 0, DateTimeKind.Utc);
+        
+        var fee = _parkingService.CalculateFee(arriveTime, leaveTime);
+        Assert.AreEqual(60, fee);
+    }
+
+    [Test]
+    public void should_be_max_150()
+    {
+         var arriveTime = new DateTime(2024, 1, 9, 0, 0, 0, 0, DateTimeKind.Utc);
+        var leaveTime = new DateTime(2024, 1, 10, 0, 0, 0, 0, DateTimeKind.Utc);
+        
+        var fee = _parkingService.CalculateFee(arriveTime, leaveTime);
+        Assert.AreEqual(150, fee);
+    }
+
 }
 
 public class ParkingService
 {
     public decimal CalculateFee(DateTime arriveTime, DateTime leaveTime)
     {
-        if (leaveTime - arriveTime >= TimeSpan.FromMinutes(30))
-        {
-            return 30m;
-        }
-
-        return 0m;
+        var timeSpan = leaveTime - arriveTime;
+        var spans = Math.Floor(timeSpan.TotalMinutes/30);
+        return decimal.Min(new decimal(spans* 30), 150m); ;
     }
 }
